e85192aa92ef5ebc45d19f9b0179e5a1e204d80a
maribelmullins <33381362+maribelmullins@users.noreply.github.com>
2022-09-23 00:05:46 -0700

    Update article-preview.js

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -7,8 +7,8 @@ import styles from './article-preview.module.css'
 export default ({ article }) => (
   <div className={styles.preview}>
     {article.cloudinaryimage && (article.cloudinaryimage[0].resource_type === "video"
-      ? <video autoPlay muted loop src={article.cloudinaryimage[0].secure_url}/>
-      : <img src={article.cloudinaryimage[0].secure_url} />)}
+      ? <video autoPlay muted loop src={(article.cloudinaryimage[0].derived || article.cloudinaryimage[0]).secure_url}/>
+      : <img src={(article.cloudinaryimage[0].derived || article.cloudinaryimage[0]).secure_url} />)}
     {article.heroImage && (<Img alt="" fluid={article.heroImage.fluid} />)}
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
